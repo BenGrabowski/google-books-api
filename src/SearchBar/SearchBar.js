d48ba239d9c87e91e76c81ff8e002c2302c35857
Ben Grabowski <gbowski30@gmail.com>
2019-08-10 11:04:23 -0600

    added ref to input

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+    
     handleSubmit(e) {
         e.preventDefault();
-        this.props.handleSearch(e.target.value);
+        this.props.handleSearch(this.myRef);
     }
     
     render() {
         return (
             <div className="SearchBar">
                 <form onSubmit={e => this.handleSubmit(e)}>
-                <label htmlFor="search-term">Search:</label>
-                <input
-                    type="text"
-                    name="search"
-                    id="search" />
-                <button type="submit">Search</button>
+                    <label htmlFor="search-term">Search:</label>
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        ref={this.myRef} />
+                    <button type="submit">Search</button>
                 </form>
             </div>
         );
